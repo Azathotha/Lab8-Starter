96a2c9bb3e8cb9f6292299be5e6ec7a33138ed63
Azathotha <93633837+Azathotha@users.noreply.github.com>
2024-05-25 19:42:33 -0700

    put url

--- sw.js
+++ sw.js
@@ -48,13 +48,13 @@ self.addEventListener("fetch", function (event) {
         // B8. TODO - If the request is in the cache, return with the cached version.
         //            Otherwise fetch the resource, add it to the cache, and return
         //            network response.
-        return (
-          response ||
-          fetch(event.request).then(function (networkResponse) {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request).then(function (networkResponse) {
             cache.put(event.request, networkResponse.clone());
             return networkResponse;
           })
-        );
       });
     })
   );
