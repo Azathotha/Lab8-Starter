85b677c415b1d4ad2d063752df8ac6f9c24a6098
Azathotha <93633837+Azathotha@users.noreply.github.com>
2024-05-25 19:57:39 -0700

    fix chrome cache

--- sw.js
+++ sw.js
@@ -43,18 +43,23 @@ self.addEventListener("fetch", function (event) {
   // B7. TODO - Respond to the event by opening the cache using the name we gave
   //            above (CACHE_NAME)
   event.respondWith(
-    caches.open(CACHE_NAME).then(function (cache) {
-      return cache.match(event.request).then(function (response) {
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.match(event.request).then((cachedResponse) => {
         // B8. TODO - If the request is in the cache, return with the cached version.
         //            Otherwise fetch the resource, add it to the cache, and return
         //            network response.
-        if (response) {
-          return response;
+        if (cachedResponse) {
+          return cachedResponse;
         }
-        return fetch(event.request).then(function (networkResponse) {
-            cache.put(event.request, networkResponse.clone());
-            return networkResponse;
+        return (
+          cachedResponse ||
+          fetch(event.request.url).then((fetchedResponse) => {
+            if (event.request.url.startsWith('http')) {
+              cache.put(event.request, fetchedResponse.clone());
+            }
+            return fetchedResponse;
           })
+        );
       });
     })
   );
